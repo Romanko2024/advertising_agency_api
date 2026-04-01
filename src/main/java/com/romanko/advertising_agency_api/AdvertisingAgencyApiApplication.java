fdbd281610a0b5063f0e8cfb5ea7002955791bd2
Romanko2024 <Roman_ko2024@outlook.com>
2026-04-01 15:37:43 +0300

    ---

---
+++ src/main/java/com/romanko/advertising_agency_api/AdvertisingAgencyApiApplication.java
@@ -0,0 +1,13 @@
+package com.romanko.advertising_agency_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingAgencyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisingAgencyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romanko/advertising_agency_api/AdvertisingAgencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanko.advertising_agency_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingAgencyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
